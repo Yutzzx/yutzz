0479df772cdb4b1ee280d3930b925695e551b682
Yutzzx <foxesskymusic@gmail.com>
2026-04-14 17:07:45 +0700

    Create yutzz_combined.lua with combined features from hitbox and ESP scripts

---
+++ yutzz_combined.lua
@@ -0,0 +1,18 @@
+-- Combined features from Hitbox and ESP scripts
+
+-- Function to display hitboxes
+function displayHitboxes()
+    -- Code to display hitboxes goes here
+end
+
+-- Function to display ESP
+function displayESP()
+    -- Code to display ESP goes here
+end
+
+-- Main loop to run the display functions
+while true do
+    displayHitboxes()
+    displayESP()
+    wait(0.1) -- Wait for a short period before the next iteration
+end
